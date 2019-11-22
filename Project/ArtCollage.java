074a6f3fd29cc3bb98b36a2c84a34be42b0791af
steven <stevenzamora205@gmail.com>
2019-11-22 17:20:39 -0500

    Minor changes to the projet

--- Project/ArtCollage.java
+++ Project/ArtCollage.java
@@ -1,3 +1,4 @@
+
 /*************************************************************************
  *  Compilation:  javac ArtCollage.java
  *  Execution:    java ArtCollage
@@ -21,35 +22,39 @@ public class ArtCollage {
 
     // A tile consists of tileDimension X tileDimension pixels
     private int tileDimension;
-    
+
     /*
-     * One-argument Constructor
-     * 1. set default values of collageDimension to 4 and tileDimension to 100
-     * 2. initializes original with the filename image
-     * 3. initializes collage as a Picture of tileDimension*collageDimension x tileDimension*collageDimension, 
-     *    where each pixel is black (see all constructors for the Picture class).
-     * 4. update collage to be a scaled version of original (see scaling filter on Week 9 slides)
+     * One-argument Constructor 1. set default values of collageDimension to 4 and
+     * tileDimension to 100 2. initializes original with the filename image 3.
+     * initializes collage as a Picture of tileDimension*collageDimension x
+     * tileDimension*collageDimension, where each pixel is black (see all
+     * constructors for the Picture class). 4. update collage to be a scaled version
+     * of original (see scaling filter on Week 9 slides)
      *
      * @param filename the image filename
      */
-    public ArtCollage (String filename) {
+    public ArtCollage(String filename) {
 
-	// WRITE YOUR CODE HERE
+        collageDimension = 4;
+        tileDimension = 100;
+
     }
 
     /*
-     * Three-arguments Constructor
-     * 1. set default values of collageDimension to cd and tileDimension to td
-     * 2. initializes original with the filename image
-     * 3. initializes collage as a Picture of tileDimension*collageDimension x tileDimension*collageDimension, 
-     *    where each pixel is black (see all constructors for the Picture class).
-     * 4. update collage to be a scaled version of original (see scaling filter on Week 9 slides)
+     * Three-arguments Constructor 1. set default values of collageDimension to cd
+     * and tileDimension to td 2. initializes original with the filename image 3.
+     * initializes collage as a Picture of tileDimension*collageDimension x
+     * tileDimension*collageDimension, where each pixel is black (see all
+     * constructors for the Picture class). 4. update collage to be a scaled version
+     * of original (see scaling filter on Week 9 slides)
      *
      * @param filename the image filename
      */
-    public ArtCollage (String filename, int td, int cd) {
+    public ArtCollage(String filename, int td, int cd) {
 
-	// WRITE YOUR CODE HERE
+        collageDimension = cd;
+        tileDimension = td;
+        original = filename;
     }
 
     /*
@@ -59,7 +64,7 @@ public class ArtCollage {
      */
     public int getCollageDimension() {
 
-	// WRITE YOUR CODE HERE
+        return collageDimension;
     }
 
     /*
@@ -69,7 +74,7 @@ public class ArtCollage {
      */
     public int getTileDimension() {
 
-	// WRITE YOUR CODE HERE
+        return getTileDimension();
     }
 
     /*
@@ -79,7 +84,7 @@ public class ArtCollage {
      */
     public Picture getOriginalPicture() {
 
-	// WRITE YOUR CODE HERE
+        return original;
     }
 
     /*
@@ -89,80 +94,81 @@ public class ArtCollage {
      */
     public Picture getCollagePicture() {
 
-	// WRITE YOUR CODE HERE
+        return collage;
     }
-    
+
     /*
-     * Display the original image
-     * Assumes that original has been initialized
+     * Display the original image Assumes that original has been initialized
      */
     public void showOriginalPicture() {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
 
     /*
-     * Display the collage image
-     * Assumes that collage has been initialized
+     * Display the collage image Assumes that collage has been initialized
      */
     public void showCollagePicture() {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
 
     /*
-     * Replaces the tile at collageCol,collageRow with the image from filename
-     * Tile (0,0) is the upper leftmost tile
+     * Replaces the tile at collageCol,collageRow with the image from filename Tile
+     * (0,0) is the upper leftmost tile
      *
      * @param filename image to replace tile
+     * 
      * @param collageCol tile column
+     * 
      * @param collageRow tile row
      */
-    public void replaceTile (String filename,  int collageCol, int collageRow) {
+    public void replaceTile(String filename, int collageCol, int collageRow) {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
-    
+
     /*
-     * Makes a collage of tiles from original Picture
-     * original will have collageDimension x collageDimension tiles, each tile
-     * has tileDimension X tileDimension pixels
+     * Makes a collage of tiles from original Picture original will have
+     * collageDimension x collageDimension tiles, each tile has tileDimension X
+     * tileDimension pixels
      */
-    public void makeCollage () {
+    public void makeCollage() {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
 
     /*
-     * Colorizes the tile at (collageCol, collageRow) with component 
-     * (see Week 9 slides, the code for color separation is at the 
-     *  book's website)
+     * Colorizes the tile at (collageCol, collageRow) with component (see Week 9
+     * slides, the code for color separation is at the book's website)
      *
      * @param component is either red, blue or green
+     * 
      * @param collageCol tile column
+     * 
      * @param collageRow tile row
      */
-    public void colorizeTile (String component,  int collageCol, int collageRow) {
+    public void colorizeTile(String component, int collageCol, int collageRow) {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
 
     /*
-     * Greyscale tile at (collageCol, collageRow)
-     * (see Week 9 slides, the code for luminance is at the book's website)
+     * Greyscale tile at (collageCol, collageRow) (see Week 9 slides, the code for
+     * luminance is at the book's website)
      *
      * @param collageCol tile column
+     * 
      * @param collageRow tile row
      */
 
-    public void greyscaleTile (int collageCol, int collageRow) {
+    public void greyscaleTile(int collageCol, int collageRow) {
 
-	// WRITE YOUR CODE HERE
+        // WRITE YOUR CODE HERE
     }
 
-
-    // Test client 
-    public static void main (String[] args) {
+    // Test client
+    public static void main(String[] args) {
 
     }
 }
